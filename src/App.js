b37631cb2151bf061257ae00d984103bb12fa02b
jimmyspann <jimmyleespann@outlook.com>
2021-06-27 18:02:14 -0400

    initialize

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Jimmy Spann</h1>
+        <p>Short Introduction</p>
+      </header>
+      <section>
+        <h2>Skills</h2>
+      </section>
+      <section>
+        <h2>About Me</h2>
+      </section>
+      <section>
+        <h2>Projects</h2>
+      </section>
+      <section>
+        <h2>Contact</h2>
+        <p>socials, and email</p>
+        <p>Maybe add resume here</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
