0b20cf7e0252af8ff7c14b2e5055af910109fcc4
jimmyspann <jimmyleespann@outlook.com>
2021-06-27 23:39:20 -0400

    Add images and object data for project cards

---
+++ src/pages/portfolio/projectsData.js
@@ -0,0 +1,49 @@
+import asb1 from "../../images/asb-1.png"
+import asb2 from "../../images/asb-2.png"
+
+import bmi1 from "../../images/bmi-1.png"
+import bmi2 from "../../images/bmi-2.png"
+import bmi3 from "../../images/bmi-3.png"
+import bmi4 from "../../images/bmi-4.png"
+import bmi5 from "../../images/bmi-5.png"
+
+import jm1 from "../../images/jm-1.png"
+import jm2 from "../../images/jm-2.png"
+import jm3 from "../../images/jm-3.png"
+
+
+const projects = [
+    {
+        title: "JM Project",
+        description: "Start up android application",
+        link: "",
+        pictures: [
+                    {src: jm1, alt: "jm1"},
+                    {src: jm2, alt: "jm2"},
+                    {src: jm3, alt: "jm3"},
+                  ]
+    },
+    {
+        title: "Alimagham Sound Board",
+        description: "etrwerwewedfwedwedwe",
+        link: "",
+        pictures: [
+                    {src: asb1, alt: "phone"},
+                    {src: asb2, alt: "phone2"},
+                  ]
+    },
+    {
+        title: "BMI Project",
+        description: "This was a college capstone project",
+        link: "",
+        pictures: [
+                    {src: bmi1, alt: "bmi1"},
+                    {src: bmi2, alt: "bmi2"},
+                    {src: bmi3, alt: "bmi3"},
+                    {src: bmi4, alt: "bmi4"},
+                    {src: bmi5, alt: "bmi5"},
+                  ]
+    }
+]
+
+export default projects;
